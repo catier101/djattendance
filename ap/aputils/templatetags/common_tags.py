d719c271a188f1543645806c116ec8d3db1255b1
Lisa Dina <lisadina@ADs-MacBook-Air.local>
2016-06-02 16:24:24 -0700

    minor changes

--- ap/aputils/templatetags/common_tags.py
+++ ap/aputils/templatetags/common_tags.py
@@ -4,5 +4,6 @@ from aputils.utils import is_trainee, is_TA
 register = template.Library()
 
 register.filter('is_trainee', is_trainee)
+
 register.filter('is_TA', is_TA)
 
--- ap/exams/context_processors.py
+++ ap/exams/context_processors.py
@@ -1,30 +1,6 @@
 from .models import Exam
-import collections
-
-def namedtuple_d(typename, field_names, default_values=()):
-    T = collections.namedtuple(typename, field_names)
-    T.__new__.__defaults__ = (None,) * len(T._fields)
-    if isinstance(default_values, collections.Mapping):
-        prototype = T(**default_values)
-    else:
-        prototype = T(*default_values)
-    T.__new__.__defaults__ = tuple(prototype)
-    return T
-
-# site: http://stackoverflow.com/questions/11351032/named-tuple-and-optional-keyword-arguments
-
-
-MenuItem = namedtuple_d('MenuItem', 'name ta_only trainee_only common specific')
-SubMenuItem = namedtuple_d('SubMenuItem', 'name permission url')
-
-exam_menu = MenuItem(name = 'Exams', 
-	# trainee_only=[SubMenuItem(name="Take Exam", url='exams:list')], 
-	specific = [SubMenuItem(name="Create Exam", permission='exams.add_exam', url='exams:submit')])
 
 def exams_available(request):
-	if Exam.objects.filter(is_open=True).count() > 0:
-		return {'exams_available' : True, 'exam_menu': exam_menu}
-	else:
-		return {'exams_available': False }
+	return {'exams_available' : Exam.objects.filter(is_open=True).count() > 0}
 
 
